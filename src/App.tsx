1e917076047a9cb886b8f0aea2094509a2151ddd
rectanglehihat <rectanglehihat@gmail.com>
2024-05-28 20:53:08 +0900

    Fix: 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
 import React from 'react';
 
 const App = () => {
-  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [previewImage, setPreviewImage] = React.useState<string>('');
+  const [fileSize, setFileSize] = React.useState({ origin: 0, compress: 0 });
+  const $inputRef = React.useRef<HTMLInputElement>(null);
 
-  const onClickUploader = () => {
-    if (!inputRef.current) return;
-    inputRef.current?.click();
+  const onChangeFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+    const quality = 0.7;
+    try {
+      compressImage(file, quality);
+    } catch (error) {
+      console.error('error', error);
+    }
+  };
+
+  const onClickUploadButton = () => {
+    if (!$inputRef.current) return;
+    $inputRef.current.click();
+  };
+
+  const compressImage = (file: File, quality: number): void => {
+    const reader = new FileReader();
+    const img = new Image();
+
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      ctx?.drawImage(img, 0, 0);
+      canvas.toBlob(
+        blob => {
+          if (!blob) return;
+          const resizedImage = URL.createObjectURL(blob);
+          setPreviewImage(resizedImage);
+          setFileSize({ origin: file.size, compress: blob.size });
+        },
+        'image/jpeg',
+        quality,
+      );
+    };
+
+    reader.onload = e => {
+      img.src = e.target?.result as string;
+    };
+
+    return reader.readAsDataURL(file);
   };
 
   return (
     <>
       <input
         className="file-input"
-        ref={inputRef}
+        ref={$inputRef}
         type="file"
         accept="image/*"
+        onChange={onChangeFileInput}
       />
-      <button onClick={onClickUploader}>Upload File</button>
-      <div id="preview">미리보기</div>
-      <div id="origin">기존 용량</div>
-      <div id="compress">압축 용량</div>
+      <button className="upload-button" onClick={onClickUploadButton}>
+        Upload File
+      </button>
+      <img className="preview-image" src={previewImage} alt="preview-image" />
+      <div className="origin-size">기존 용량: {fileSize.origin}</div>
+      <div className="compress-size">압축 용량: {fileSize.compress}</div>
     </>
   );
 };

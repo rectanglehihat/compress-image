76c950447004e3ba37d8e4406a2a49c5bf61186d
rectanglehihat <rectanglehihat@gmail.com>
2024-05-29 23:42:59 +0900

    Fix: webpack 충돌 수정

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,13 +3,5 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
   plugins: [new Dotenv({ path: './env/.env.development' })],
 };

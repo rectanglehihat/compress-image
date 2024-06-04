917bf626dc9482875316555d70b7200dacb0a287
rectanglehihat <rectanglehihat@gmail.com>
2024-06-04 20:06:46 +0900

    Fix: 함수 compress.ts 파일로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,5 @@
 import React, { ChangeEvent, useRef, useState } from 'react';
-
-const compressImage = (
-  file: File,
-  quality: number,
-  type: string,
-): Promise<{
-  resizedImage: string;
-  fileSize: { origin: number; compress: number };
-}> => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    const img = new Image();
-
-    img.onload = () => {
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-
-      canvas.width = img.width;
-      canvas.height = img.height;
-
-      if (ctx) {
-        ctx.drawImage(img, 0, 0);
-        canvas.toBlob(
-          blob => {
-            if (!blob) {
-              reject(new Error('Failed to create blob from canvas'));
-              return;
-            }
-            const resizedImage = URL.createObjectURL(blob);
-            const fileSize = { origin: file.size, compress: blob.size };
-            resolve({ resizedImage, fileSize });
-          },
-          `image/${type || '*'}`,
-          quality,
-        );
-      } else {
-        reject(new Error('Failed to get canvas context'));
-      }
-    };
-
-    reader.onload = e => {
-      img.src = e.target?.result as string;
-    };
-
-    reader.onerror = () => {
-      reject(new Error('Failed to read file'));
-    };
-
-    reader.readAsDataURL(file);
-  });
-};
+import compress from './compress.ts';
 
 interface AppProps {
   width?: number;
@@ -57,6 +7,7 @@ interface AppProps {
   quality?: number;
   type?: string;
   style?: string;
+  accept?: string;
 }
 
 const App = ({
@@ -64,6 +15,7 @@ const App = ({
   height = 300,
   quality = 0.7,
   type = 'jpeg',
+  accept = 'image/*',
 }: AppProps) => {
   const [previewImage, setPreviewImage] = useState<string>('');
   const [fileSize, setFileSize] = useState({ origin: 0, compress: 0 });
@@ -74,11 +26,7 @@ const App = ({
     const file = e.target.files[0];
 
     try {
-      const { resizedImage, fileSize } = await compressImage(
-        file,
-        quality,
-        type,
-      );
+      const { resizedImage, fileSize } = await compress(file, quality, type);
       setPreviewImage(resizedImage);
       setFileSize(fileSize);
     } catch (error) {
@@ -87,8 +35,7 @@ const App = ({
   };
 
   const onClickUploadButton = () => {
-    if (!$inputRef.current) return;
-    $inputRef.current.click();
+    $inputRef.current?.click();
   };
 
   return (
@@ -97,7 +44,7 @@ const App = ({
         className="file-input"
         ref={$inputRef}
         type="file"
-        accept="image/*"
+        accept={accept}
         onChange={onChangeFileInput}
       />
       <button className="upload-button" onClick={onClickUploadButton}>
